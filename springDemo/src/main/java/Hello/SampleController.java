2a34a515d6dab705452f42c1d43507702c3569bd
zhangcheng170245 <170902823@qq.com>
2019-05-19 23:31:38 +0800

    Rejected commit: Httpclient

--- springDemo/src/main/java/Hello/SampleController.java
+++ springDemo/src/main/java/Hello/SampleController.java
@@ -1,7 +1,27 @@
 package Hello;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 /**
  * Created by zhangcheng on 2019/5/12.
  */
+@Controller
+@EnableAutoConfiguration
 public class SampleController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SampleController.class, args);
+    }
+
 }
